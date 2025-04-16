- fixed mute buttons scaling depending on row count - refactored mute button

tools/bitmap_knobs.py
@@ -271,9 +271,42 @@ def dial_knob(filepath: str | Path | None = 'dial_knob.png',
     im.save(str(filepath))
     return filepath
 
-# rotary_knob('rotary_knob.png', fg_r=1.5, bg_r=1.5, dot_r=4, bg_move=True, start_end_hide=True)
-# dial_knob('dial_knob.png', bg_r=None, bg_stroke_r=None, mark_r=2, mark_p=(12, 32), scl=2.5, frames=5,
-#           bg_color='7f7f7f7f')
-# linear_slider(shape=(16, 160), dot_r=4)
-# result = gamma_rgb(result, 2.0)
-# result = grade_rgb(result, (.25, .5, 1.), (1., .5, .25))
+
+def round_button(filepath: str | Path | None = 'round_button.png',
+                 size: int = 64, margin: float = 1, scl: float = 1,
+                 bg_r: float | None = None,
+                 bg_color: str = 'ffffffff'):
+    """
+    Render a basic round button
+
+    :param filepath: Return array if None
+
+    :param size: Image size
+    :param margin: Margin around the image in pixels
+    :param scl: Scale factor
+
+    :param bg_r: radius in pixels
+
+    :param bg_color: argb hex color
+
+    :return: Image path or array
+    """
+    shape = np.abs(np.array((size, size)) * scl).astype(np.uint).tolist()
+    sx, sy = shape
+
+    p = np.array([sx / 2 - .5] * 2)
+
+    max_r = sx / 2 - margin
+    r = min((bg_r, max_r)[bg_r is None], max_r)
+
+    df = point_df(shape, p) - r
+    alpha = test_df(df, 0)
+
+    result = extend_edge(fill_alpha(alpha, plt_to_rgba(bg_color)))
+
+    if not filepath:
+        return result
+
+    im = np_to_pil(result)
+    im.save(str(filepath))
+    return filepath

tools/smp_to_dspreset.py
@@ -17,7 +17,7 @@ from xml.dom import minidom
 
 import instrument_utils as iu
 from bitmap_knobs import *
-from color_utils import (adjust_palette, basic_background, basic_button, blank_button, write_pil_image,
+from color_utils import (adjust_palette, basic_background, blank_button, write_pil_image,
                          write_text, limit_font_size, apply_symbol, hex_to_rgba, rgba_to_hex, plt_to_rgba)
 from common_math_utils import linstep, lerp, clamp
 from common_ui_utils import shorten_str, beautify_str
@@ -288,12 +288,11 @@ def create_dspreset(root_dir: str, smp_subdir: str = 'Samples',
     btn_w = 16
     if add_grp_knobs:
         for i, plt in enumerate([track_bg_plt[0], mute_btn_plt[0]]):
-            rgba = np.array([1, 1, 1, 1]) * np.roll(hex_to_rgba(plt), -1).tolist()
             btn_path = resources_dir / f'grp_btn{i}.png'
-            btn_path = basic_button(str(btn_path), size=btn_w * 2, rgba=rgba)
+            btn_path = round_button(btn_path, size=btn_w, bg_color=plt, scl=2)
             btn_path = Path(btn_path).relative_to(root_dir)
             btn_paths.append(str(btn_path.as_posix()))
-    btn_w /= row_spc_adjust
+    # btn_w /= row_spc_adjust
 
     # Round-Robin offsets (Fake Round-Robin)
     rr_offset = rr_offset or [0]
@@ -893,6 +892,7 @@ def create_dspreset(root_dir: str, smp_subdir: str = 'Samples',
                 ctrls.append(ctrlname)
 
                 # Mute button
+                btn_w = (ctrl_w / 80) * 16
                 btn = Et.SubElement(tab, 'button',
                                     attrib={'x': str(x + (ctrl_w - btn_w) // 2), 'y': str(y + ctrl_w - btn_w // 2),
                                             'style': 'image', 'width': str(btn_w), 'height': str(btn_w),